41f1c9d36f3c79ad6192ac72353394d3832a868b
armaanv <arm0206@gmail.com>
2015-10-31 16:11:35 -0700

    I am a Pokemon master.

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,10 +1,11 @@
-class PokemonsController < ApplicationController
+class PokemonController < ApplicationController
   def capture
     @pokemon = Pokemon.find(params[:id])
     @pokemon.trainer = current_trainer
     @pokemon.save
     redirect_to root_path
   end
+
   def damage
     pokemon = Pokemon.find(params[:id])
     pokemon.health -= 10
@@ -14,6 +15,7 @@ class PokemonsController < ApplicationController
     end
     redirect_to pokemon.trainer
   end
+
   def create
 		@pokemon = Pokemon.create pokemon_params
 		@pokemon.health = 100
@@ -23,11 +25,23 @@ class PokemonsController < ApplicationController
 		  redirect_to trainer_path(current_trainer)
 		else
 	      flash[:error] = @pokemon.errors.full_messages.to_sentence
+        redirect_to new_pokemon_path
 	  end
 	end
-  def new
-    #@pokemon = Pokemon.create( name: params[:name], health: 100, level: 1, trainer: current_trainer)
+
+  def heal
+    pokemon = Pokemon.find(params[:id])
+    pokemon.health += 10
+    pokemon.save
+    redirect_to pokemon.trainer
   end
+
+  def new
+    #@pokemon = Pokemon.create(params.require(:pokemon).permit(:name))
+    #redirect_to trainer_path(current_trainer)
+    @pokemon = Pokemon.new
+  end
+
 	def pokemon_params
 		params.require(:pokemon).permit(:name)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
-  patch 'capture', to: 'pokemons#capture'
-  patch 'damage', to: 'pokemosn#damage'
-  post '/create', to:'pokemons#create'
-  get '/new', to:'pokemons#new'
+  patch 'capture', to: 'pokemon#capture'
+  patch 'damage', to: 'pokemon#damage'
+  patch 'heal', to: 'pokemon#heal'
+  post '/create', to:'pokemon#create'
+  get '/new', to:'pokemon#new'
 end
